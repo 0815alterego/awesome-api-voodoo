b1cc29a2951dfe25a6ace0cc23483e7903c7f7cf
paul.stange@gmail.com <3inF@ches!>
2016-02-14 00:24:37 +0100

    inserted token for openVuln API

--- vulni.py
+++ vulni.py
@@ -8,8 +8,14 @@ import xml.etree.ElementTree as et
 def getdata():
     response = requests.get('https://api.cisco.com/security/advisories/cvrf/latest/10.xml', headers={'Authorization':'Bearer '+kacktoken.kacktoken()})
     tree = et.fromstring(response.content)
-    cve_number = tree.findall(".//cve")
-    cve_url = tree.findall(".//cvrfUrl")
-    for cve in cve_number:
-        print(cve.)
+    cve_ids = tree.findall(".//advisoryId")
+    cve_urls = tree.findall(".//cvrfUrl")
+    #print(len(cve_ids))
+    #print(len(cve_urls))
+    length = len(cve_ids)
+    for i in range(length):
+        #print(cve.text)
+        dict={"ID":cve_ids[i].text,"URI":cve_urls[i].text}
+        length = length-1
+
 getdata()
