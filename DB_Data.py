1a81b7d9bc6c778a2a578ea16e4b4a1a63041a5e
Aschrad <mob_github@unimatrix01.de>
2016-02-14 10:38:16 +0100

    Calling Tropop and generating msg for psirt

--- DB_Data.py
+++ DB_Data.py
@@ -8,4 +8,4 @@ Device_type = {'ASA': [101, 102, 400],
 
 User_ID = {101: {'name':'Paul Stange','mail':'paul.spamm1@unimatrix01.de','phone':'12345670'},
            102: {'name':'Michael Dombek','mail':'spamm@unimatrix01.de','phone':'12345670'},
-           400: {'name':'Test User','mail':'spamm_3@unimatrix01.de','phone':'12345670'}}
+           400: {'name':'Test User','mail':'spam    m_3@unimatrix01.de','phone':'12345670'}}
